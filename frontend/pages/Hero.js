abee446e28111523c361d716b94023c3b0edcd02
Sunday Mgbogu <digitalsimboja@gmail.com>
2022-09-09 20:10:58 +0100

    Hero page updated

--- frontend/pages/Hero.js
+++ frontend/pages/Hero.js
@@ -1,12 +1,63 @@
-import styles from "../styles/Hero.module.css";
+import Head from 'next/head';
+import {
+  Box,
+  Heading,
+  Container,
+  Text,
+  Button,
+  Stack,
+  Icon,
+  useColorModeValue,
+  createIcon,
+} from '@chakra-ui/react';
 
-export default function Hero() {
-    return (
-        <div className={styles.main}>
-            <div className={styles.header}>
+export default function CallToActionWithAnnotation() {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
 
-            </div>
-
-        </div>
-    )
+      <Container maxW={'3xl'}>
+        <Stack
+          as={Box}
+          textAlign={'center'}
+          spacing={{ base: 8, md: 14 }}
+          py={{ base: 20, md: 36 }}>
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
+            lineHeight={'110%'}>
+            Connect your wallet<br />
+            <Text as={'span'} color={'green.400'}>
+              to view all your NFT collections
+            </Text>
+          </Heading>
+          <Text color={'gray.500'}>
+            Powered by Rainbowkit Wallet Address Manager
+          </Text>
+          <Stack
+            direction={'column'}
+            spacing={3}
+            align={'center'}
+            alignSelf={'center'}
+            position={'relative'}>
+            <Button
+              colorScheme={'green'}
+              bg={'green.400'}
+              rounded={'full'}
+              px={6}
+              _hover={{
+                bg: 'green.500',
+              }}>
+              Connect Wallet
+            </Button>
+          </Stack>
+        </Stack>
+      </Container>
+    </>
+  );
 }
