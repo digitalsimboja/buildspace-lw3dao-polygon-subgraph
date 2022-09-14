65e69a4d9ae0bc61307d153ebc723c161f215117
Sunday Mgbogu <digitalsimboja@gmail.com>
2022-09-14 21:20:56 +0100

    NFT listing on the walletId page testing

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -32,7 +32,7 @@ export default function Header() {
         gap={4}
         w={"full"}
       >
-        <GridItem colSpan={1}>
+        <GridItem colSpan={1} >
           <VStack alignItems="flex-start" spacing="20px">
             <Image
               borderRadius="full"
@@ -56,7 +56,6 @@ export default function Header() {
           </Flex>
         </GridItem>
       </Grid>
-      <Divider mt={12} mb={12} />
     </Box>
   );
 }
--- frontend/pages/profiles/[walletId].js
+++ frontend/pages/profiles/[walletId].js
@@ -1,12 +1,10 @@
 import {
-  Avatar,
   Box,
   Container,
   chakra,
   Grid,
   Progress,
   GridItem,
-  Stack,
   Divider,
 } from "@chakra-ui/react";
 import { useRouter } from "next/router";
@@ -14,17 +12,13 @@ import { useRouter } from "next/router";
 import { useQuery, gql } from "@apollo/client";
 import Navbar from "../../components/Navbar";
 import Header from "../../components/Header";
-import { erc721ABI, useAccount, useContract, useProvider } from "wagmi";
-
-import {
-  BUILDSPACE_ADDRESS,
-  LEARNWEB3DAOGRADUATENFT_ADDRESS,
-} from "../../constants";
+import { useProvider } from "wagmi";
+import { useEffect } from "react";
 
 // Get the provider, connected address and a contract instance
 // for the NFT contract using wagmi
 
-/*/ The GraphQL query to run
+//The GraphQL query to run
 const profilesQuery = gql`
   query fetchUserSkillsNftsEnitites {
     users(id: $id) {
@@ -38,13 +32,11 @@ const profilesQuery = gql`
     }
   }
 `;
-*/
+
 
 // Fetch NFT details from the contract
-async function updateTokenURI(nftContract) {
+async function updateTokenURI(tokenURI) {
   try {
-    // Get token URI from contract
-    let tokenURI = await nftContract.tokenURI(0);
 
     // If it's an IPFS URI, replace it with an HTTP Gateway link
     tokenURI = await tokenURI.replace("ipfs://", "https://ipfs.io/ipfs/");
@@ -77,50 +69,6 @@ const DisplayBLDSpaceNFT = ({ organization, tokenURI }) => {
 };
 
 const DisplayLearnWeb3NFT = ({ organization, tokenURI }) => {
-  /*
-  const provider = useProvider();
-
-   // The contract instance for both Buildspace and Learnweb3
- const LearnWeb3Contract = useContract({
-  addressOrName: LEARNWEB3DAOGRADUATENFT_ADDRESS,
-  contractInterface: erc721ABI,
-  signerOrProvider: provider,
-});
-
-const BuildSpaceContract = useContract({
-  addressOrName: BUILDSPACE_ADDRESS,
-  contractInterface: erc721ABI,
-  signerOrProvider: provider,
-});
-
- 
- 
-  if (organization === "LearnWeb3GraduatesNFT") {
-    let image =  updateTokenURI(LearnWeb3Contract);
-    return (
-      <GridItem>
-        <chakra.h3 fontSize="xl" fontWeight="600">
-          {organization}
-        </chakra.h3>
-        <chakra.p>{image}</chakra.p>
-      </GridItem>
-    );
-
-  } if (organization === "buildspace") {
-
-    let image = updateTokenURI(BuildSpaceContract);
-    return (
-      <GridItem>
-        <chakra.h3 fontSize="xl" fontWeight="600">
-          {organization}
-        </chakra.h3>
-        <chakra.p>{image}</chakra.p>
-      </GridItem>
-    );
-
-    
-  }
-  */
   return (
     <GridItem>
       <chakra.h3 fontSize="xl" fontWeight="600">
@@ -130,21 +78,13 @@ const BuildSpaceContract = useContract({
     </GridItem>
   );
 };
+/*
+useEffect(() => {
+  //Update the tokenURI based on who issued it
+  //updateURI(tokenURI)
 
-// The GraphQL query to run
-const profilesQuery = gql`
-  query fetchUserSkillsNftsEnitites {
-    users(where: { id: "0x083fe503ea4e6319bf5fd710316124a36e13bda9" }) {
-      id
-      skillNFTs {
-        id
-        organization
-        tokenURI
-        tokenId
-      }
-    }
-  }
-`;
+}, [])
+*/
 
 export default function ProofOfKnowledgeDetails() {
   const router = useRouter();
@@ -154,13 +94,9 @@ export default function ProofOfKnowledgeDetails() {
   // for the NFT contract using wagmi
   const provider = useProvider();
 
-  const goHome = async () => {
-    return router.push("/");
-  };
-
   //  Query the graph and get a response
-  //const { loading, data, error } = useQuery(profilesQuery, {variables: {id: {walletId}}});
-  const { loading, data, error } = useQuery(profilesQuery);
+  const { loading, data, error } = useQuery(profilesQuery, {variables: {id: {walletId}}});
+
 
   if (error) {
     console.log(error);
@@ -177,6 +113,8 @@ export default function ProofOfKnowledgeDetails() {
           </Box>
         ) : (
           <Box as={Container} maxW="7xl" mt={14} p={4}>
+            <Divider mt={12}  mb={10}/>
+            <chakra.h3>LearnWeb3GraduatesNFT Proof of Knowledge</chakra.h3> 
             <Grid
               templateColumns={{
                 base: "repeat(1, 1fr)",
@@ -184,6 +122,7 @@ export default function ProofOfKnowledgeDetails() {
                 md: "repeat(4, 1fr)",
               }}
               gap={{ base: "8", sm: "12", md: "16" }}
+              h={20}
             >
               {data &&
                 data.users[0]["skillNFTs"].map(
@@ -195,8 +134,10 @@ export default function ProofOfKnowledgeDetails() {
                   }
                 )}
             </Grid>
-            <Divider />
+            
+            <Divider mt={12}  mb={10}/>
 
+            <chakra.h3>BuildSpace NFTs</chakra.h3>
             <Grid
               templateColumns={{
                 base: "repeat(1, 1fr)",
@@ -204,6 +145,7 @@ export default function ProofOfKnowledgeDetails() {
                 md: "repeat(4, 1fr)",
               }}
               gap={{ base: "8", sm: "12", md: "16" }}
+              h={20}
             >
               {data &&
                 data.users[0]["skillNFTs"].map(
